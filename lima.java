ac4152dd9c23023f79dbf8f81ed927437a99618e
Wibby Bernaldi <wibbybernaldi@gmail.com>
2015-03-24 16:15:20 +0700

    Tugas Kelima Perulangan
    Perulangan angka

---
+++ lima.java
@@ -0,0 +1,10 @@
+public class lima {
+    public static void main(String[] args) {
+
+        int batas=8;
+        System.out.print("angka yang muncul : ");
+        for (int ang=1;ang<batas;ang++){
+            System.out.print(ang + ",");
+        }
+    }
+}

834c9a41f1b6fe535d9f9b335a5f64eb75514f72
Wibby Bernaldi <wibbybernaldi@gmail.com>
2015-03-24 16:39:43 +0700

    Tugas Kelima Perulangan
    Revisi

--- lima.java
+++ lima.java
@@ -1,10 +1,14 @@
 public class lima {
     public static void main(String[] args) {
 
-        int batas=8;
-        System.out.print("angka yang muncul : ");
-        for (int ang=1;ang<batas;ang++){
-            System.out.print(ang + ",");
+        int batas=10;
+        int ang=1;
+        System.out.print("angka yang muncul dengan batas "+ang+" sampai "+batas+" : ");
+        for (;ang<=batas;ang++){
+            System.out.print(ang);
+            if (ang<batas) {
+            	System.out.print(",");
+            }
         }
     }
 }

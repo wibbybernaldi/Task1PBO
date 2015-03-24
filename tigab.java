b6c38032c36bf8df9bf738639e3375c3150b9173
Wibby Bernaldi <wibbybernaldi@gmail.com>
2015-03-24 15:55:19 +0700

    Tugas Ketiga Tulis Angka
    Pakai Switch Case

---
+++ tigab.java
@@ -0,0 +1,41 @@
+
+public class tigab {
+    public static void main(String[] args) {
+        int angka=7;
+        System.out.println("Angka yang anda masukkan : " + angka);
+
+        switch (angka){
+            case 1:
+                System.out.println("satu");
+                break;
+            case 2:
+                System.out.println("dua");
+                break;
+            case 3:
+                System.out.println("tiga");
+                break;
+            case 4:
+                System.out.println("empat");
+                break;
+            case 5:
+                System.out.println("lima");
+                break;
+            case 6:
+                System.out.println("enam");
+                break;
+            case 7:
+                System.out.println("tujuh");
+                break;
+            case 8:
+                System.out.println("delapan");
+                break;
+            case 9:
+                System.out.println("sembilan");
+                break;
+            default:
+                System.out.println("Maaf hanya sampai 9");
+        }
+
+
+    }
+}

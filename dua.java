496138e8e280487729ea796982e87147420df925
Wibby Bernaldi <wibbybernaldi@gmail.com>
2015-03-24 15:18:43 +0700

    Tugas Kedua Angka
    Cek angka genap atau ganjil

---
+++ dua.java
@@ -0,0 +1,14 @@
+
+public class dua {
+    public static void main(String[] args) {
+
+        int angka = 15;
+
+        if (angka%2 == 0){
+            System.out.println("Angka yang anda masukkan Angka Genap");
+
+        }else{
+            System.out.println("Angka yang anda masukkan Angka Ganjil");
+        }
+    }
+}

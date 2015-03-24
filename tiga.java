fbc87cf691ca44de693e16a758a5d25c9e2a3040
Wibby Bernaldi <wibbybernaldi@gmail.com>
2015-03-24 15:42:25 +0700

    Tugas Ketiga Tulis Angka IF ELSE
    Pakai If Else

---
+++ tiga.java
@@ -0,0 +1,31 @@
+
+public class tiga {
+    public static void main(String[] args) {
+        int angka=6;
+
+        System.out.println("Angka yang anda masukkan : " + angka);
+        System.out.println("Jadi : ");
+        if (angka==1){
+            System.out.println("satu");
+        }else if (angka==2){
+            System.out.println("satu,dua");
+        }else if (angka==3){
+            System.out.println("satu,dua,tiga");
+        }else if (angka==4){
+            System.out.println("satu,dua,tiga,empat");
+        }else if (angka==5){
+            System.out.println("satu,dua,tiga,empat,lima");
+        }else if (angka==6){
+            System.out.println("satu,dua,tiga,empat,lima,enam");
+        }else if (angka==7){
+            System.out.println("satu,dua,tiga,empat,lima,enam,tujuh");
+        }else if (angka==8){
+            System.out.println("satu,dua,tiga,empat,lima,enam,tujuh,delapan");
+        }else if (angka==9){
+            System.out.println("satu,dua,tiga,empat,lima,enam,tujuh,delapan,sembilan");
+        }else {
+            System.out.println("maaf hanya sampai 9");
+        }
+
+    }
+}

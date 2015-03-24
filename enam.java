3c365d6da5b2c78a5d4954752a75bb035e45d5d9
Wibby Bernaldi <wibbybernaldi@gmail.com>
2015-03-24 16:46:01 +0700

    Tugas Keenam Penjumlahan
    Perulangan penjumlahan

---
+++ enam.java
@@ -0,0 +1,14 @@
+
+public class enam {
+    public static void main(String[] args) {
+
+        int batas=10;
+        int jumlah=0;
+        int awal=1;
+        System.out.print("jumlah angka dari "+awal+" sampai "+batas+" : ");
+        for (;awal<=batas;awal++){
+            jumlah = jumlah + awal;
+        }
+        System.out.println(jumlah);
+    }
+}

9b6814b7a242b6aa9a72ed10ee40d70f0eb12bce
Wibby Bernaldi <wibbybernaldi@gmail.com>
2015-03-25 20:50:30 +0700

    Tambahan Tugas Kesembilan
    nambah sedikit

--- sembilan.java
+++ sembilan.java
@@ -3,12 +3,15 @@ public class sembilan {
         int akhir=110;
 
         for (int i=1;i<=akhir;i++){
+            System.out.print(" ");
             if(i%5==0){
                 System.out.print("Jonny");
             }else {
                 System.out.print(i);
+            }if (i%11==0) {
+                System.out.print("\n");
             }
-            System.out.print(" ");
+            
         }
     }
 }
